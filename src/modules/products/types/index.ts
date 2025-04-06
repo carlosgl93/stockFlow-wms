7db7164864bab08b233034ff215d96f3758bee28
Carlos Gumucio <cgumuciolabbe@gmail.com>
2025-04-06 14:36:50 -0400

    fixes

--- src/modules/products/types/index.ts
+++ src/modules/products/types/index.ts
@@ -1,6 +1,8 @@
 export { Category } from "./Category";
 export { RiskCategory } from "./RiskCategory";
 export type { IProduct } from "./IProduct";
+export type { IBoxDetails } from "./IProduct";
+
 export { IPallet } from "./IProduct";
 export { IContainer } from "./IContainer";
 export { IUnitOfMeasure } from "./IUnitOfMeasure";
